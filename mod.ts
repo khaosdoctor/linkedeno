1ce3020c8e4e80c3835f5fcba520d87e7b18e00c
Lucas Santos <lhs.santoss@gmail.com>
2024-01-15 19:47:15 +0100

    fix: use barrel file
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
 export * from './src/LoginClient.ts'
 export * from './src/AuthenticatedClient.ts'
-export * as validations from './src/lib/validations.ts'
-export * as types from './src/lib/types.ts'
+export * from './src/lib/validations.ts'
+export * from './src/lib/types.ts'
