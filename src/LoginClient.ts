54368d92ac92b448c16e984fa33c90ac9ac2447e
Lucas Santos <lhs.santoss@gmail.com>
2024-01-15 20:28:59 +0100

    feat: add csrf validation
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- src/LoginClient.ts
+++ src/LoginClient.ts
@@ -3,6 +3,7 @@ import { MissingParameterError } from './errors/MissingParameter.ts'
 import { API_VERSION, LinkedinURLs, RESTLI_VERSION } from './lib/shared.ts'
 import { encodeBase64, logger } from './lib/deps.ts'
 import { accessTokenResponseSchema } from './lib/validations.ts'
+import { InvalidStateError } from './errors/InvalidState.ts'
 
 export interface LinkedinClientOptions {
   clientId: string
@@ -13,12 +14,16 @@ export interface LinkedinClientOptions {
   retryAttempts?: number
   defaultDelayBetweenRequestsMs?: number
   noRetries?: boolean
+  noValidateCSRF?: boolean
+  nonceExpirationMs?: number
 }
 
 export class LinkedinClient {
   protected readonly logger = logger
   protected readonly retries: number
   protected readonly defaultDelayBetweenRequestsMs: number
+  static sessionMap = new Set<string>()
+
   apiVersion = API_VERSION
   restliProtocolVersion = RESTLI_VERSION
 
@@ -38,6 +43,9 @@ export class LinkedinClient {
       throw new MissingParameterError(missing)
     }
   }
+  static get validSessions() {
+    return LinkedinClient.sessionMap.values()
+  }
 
   get loginUrl() {
     const url = new URL(LinkedinURLs.loginUrl)
@@ -51,10 +59,27 @@ export class LinkedinClient {
     this.logger.debug(`LinkedinClient.loginUrl :: URL ${url.toString()}`)
     this.logger.debug(`LinkedinClient.loginUrl :: nonce ${nonce}`)
 
+    if (!this.clientOptions.noValidateCSRF) {
+      LinkedinClient.sessionMap.add(nonce)
+      this.logger.debug(`LinkedinClient.loginUrl :: adding nonce ${nonce}`)
+      this.logger.debug(`LinkedinClient.loginUrl :: nonceMap ${Deno.inspect(LinkedinClient.sessionMap)}`)
+
+      setTimeout(() => {
+        this.logger.debug(`LinkedinClient.loginUrl :: removing nonce ${nonce}`)
+        this.logger.debug(`LinkedinClient.loginUrl :: nonceMap ${Deno.inspect(LinkedinClient.sessionMap)}`)
+        LinkedinClient.sessionMap.delete(nonce)
+      }, this.clientOptions.nonceExpirationMs ?? 60000)
+    }
+
     return { url: url.toString().replaceAll('+', '%20'), nonce }
   }
 
-  async exchangeLoginToken(loginToken: string) {
+  async exchangeLoginToken(loginToken: string, receivedNonce: string) {
+    if (!this.clientOptions.noValidateCSRF && !LinkedinClient.sessionMap.has(receivedNonce)) {
+      this.logger.critical(`LinkedinClient.exchangeLoginToken :: receivedNonce ${receivedNonce} not found in nonceMap`)
+      throw new InvalidStateError(receivedNonce)
+    }
+
     const response = await fetch(LinkedinURLs.getOrRefreshAccessToken, {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
---
+++ src/errors/InvalidState.ts
@@ -0,0 +1,11 @@
+import { BaseError } from './BaseError.ts'
+
+export class InvalidStateError extends BaseError {
+  constructor(nonce: string) {
+    super(`Nonce ${nonce} could not be found in the state`, {
+      name: InvalidStateError.name,
+      data: { nonce },
+      code: 'INVALID_STATE'
+    })
+  }
+}
